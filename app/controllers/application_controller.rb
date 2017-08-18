23cd162fbaf9fa0d4ad6db81c88f544a5853461f
sunshine <ashleshasingh8@gmail.com>
2017-08-18 19:42:20 +0530

    rails_admin completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+def after_sign_in_path_for (resource)
+
+	if current_user.role?('admin')
+		
+		rails_admin.dashboard_path
+	else
+	     root_path	
+	end	
+end
+
+
+
 end
