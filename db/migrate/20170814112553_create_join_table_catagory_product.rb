ca019e77f929f092c70804be81a28724f7e676d6
vaishuch <vaishu.svits@gmail.com>
2017-08-18 17:14:43 +0530

    schema file and cat_prod join

--- db/migrate/20170814112553_create_join_table_catagory_product.rb
+++ db/migrate/20170814112553_create_join_table_catagory_product.rb
@@ -1,10 +1,10 @@
 class CreateJoinTableCatagoryProduct < ActiveRecord::Migration[5.1]
   def change
     create_table :catagories_products,  id: false do |t|
-    	t.integer :catagory_id
-    	t.integer :product_id
-      # t.index [:catagory_id, :product_id]
-      # t.index [:product_id, :catagory_id]
+      t.integer :catagory_id
+      t.integer :product_id
+      t.index [:catagory_id, :product_id]
+      t.index [:product_id, :catagory_id]
     end
   end
 end
