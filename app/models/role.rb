f7f9b5fcc54aa135dc50031b2ced1db44f28cdbe
vaishuch <vaishu.svits@gmail.com>
2017-08-18 17:09:19 +0530

    role and user model edited

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,3 @@
 class Role < ApplicationRecord
-  belongs_to :user
+  has_and_belongs_to_many  :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
 
-	has_one :userinformation
-	has_many :roles
+  has_one :userinformation
+  has_and_belongs_to_many  :roles
 
-accepts_nested_attributes_for :userinformation, :allow_destroy => true
+  accepts_nested_attributes_for :userinformation, :allow_destroy => true
 
 end
