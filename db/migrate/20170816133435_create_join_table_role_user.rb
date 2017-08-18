cd785174f59b00a69af187620e22e4340eba97c2
vaishuch <vaishu.svits@gmail.com>
2017-08-18 20:00:32 +0530

    rails admin

---
+++ db/migrate/20170816133435_create_join_table_role_user.rb
@@ -0,0 +1,11 @@
+class CreateJoinTableRoleUser < ActiveRecord::Migration[5.1]
+  def change
+    create_table :roles_users,  id: false do |t|
+    t.integer :role_id
+    t.integer :user_id
+
+    t.index [:role_id, :user_id]
+    t.index [:user_id, :role_id]
+   end
+ end
+end
